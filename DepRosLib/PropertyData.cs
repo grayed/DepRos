6758b8f8a4c5a786f2c67893a2aff6dcaf96b5e1
Vadim Zhukov <persgray@gmail.com>
2025-04-11 22:40:24 +0300

    unbreak

--- DepRosLib/PropertyData.cs
+++ DepRosLib/PropertyData.cs
@@ -579,7 +579,7 @@ namespace DepRos
 
         private string GenerateWpfChangedHandler() => PropertyChangedHandlerPrototype switch {
             PropertyChangedHandlerPrototype.Unsupported => @"null",
-            PropertyChangedHandlerPrototype.Empty => $"(d, e) => (({Owner.Name})d).{PropertyChangedHandlerName}(({TypeName})e.NewValue)",
+            PropertyChangedHandlerPrototype.Empty => $"(d, e) => (({Owner.Name})d).{PropertyChangedHandlerName}()",
             PropertyChangedHandlerPrototype.NewValueOnly => $"(d, e) => (({Owner.Name})d).{PropertyChangedHandlerName}(({TypeName})e.NewValue)",
             PropertyChangedHandlerPrototype.OldAndNewValue => $"(d, e) => (({Owner.Name})d).{PropertyChangedHandlerName}(({TypeName})e.OldValue, ({TypeName})e.NewValue)",
             PropertyChangedHandlerPrototype.EventArgs => $"(d, e) => (({Owner.Name})d).{PropertyChangedHandlerName}(new(({TypeName})e.OldValue, ({TypeName})e.NewValue))",
